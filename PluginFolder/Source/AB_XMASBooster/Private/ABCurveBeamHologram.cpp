0a5b2c54da82eb31a9794a7a7ae9119c6bdeef47
davidg <david.h.gillen@gmail.com>
2022-12-07 21:47:51 -0700

    Lots of staring at comments figuring things out.

--- PluginFolder/Source/AB_XMASBooster/Private/ABCurveBeamHologram.cpp
+++ PluginFolder/Source/AB_XMASBooster/Private/ABCurveBeamHologram.cpp
@@ -6,3 +6,34 @@
 //////////////////////////////////////////////////////
 AABCurveBeamHologram::AABCurveBeamHologram() {
 }
+
+// AFGHologram interface
+//////////////////////////////////////////////////////
+void AABCurveBeamHologram::GetSupportedBuildModes_Implementation(TArray<TSubclassOf<UFGHologramBuildModeDescriptor>>& out_buildmodes) const
+{
+	Super::GetSupportedBuildModes_Implementation(out_buildmodes);
+
+	if (mBuildModeCurved) {				out_buildmodes.AddUnique(mBuildModeCurved); }
+	if (mBuildModeCompoundCurve) {		out_buildmodes.AddUnique(mBuildModeCompoundCurve); }
+}
+
+void AABCurveBeamHologram::ConfigureActor(AFGBuildable* inBuildable) const
+{
+	Super::ConfigureActor(inBuildable);
+}
+
+// Multistep
+//////////////////////////////////////////////////////
+bool AABCurveBeamHologram::DoMultiStepPlacement(bool isInputFromARelease)
+{
+	if (IsCurrentBuildMode(mBuildModeCurved)) {
+
+		return true;
+	}
+
+	if (IsCurrentBuildMode(mBuildModeCurved)) {
+		return true;
+	}
+
+	return Super::DoMultiStepPlacement(isInputFromARelease);
+}
--- PluginFolder/Source/AB_XMASBooster/Public/ABCurveBeamHologram.h
+++ PluginFolder/Source/AB_XMASBooster/Public/ABCurveBeamHologram.h
@@ -10,12 +10,41 @@
 #include "ABCurveBeamHologram.generated.h"
 
 /**
- * Provides a way to create a curvebeamololololol
+ * Provides a way to create a curvebeam by adding further buildmodes and steps to a regular beam
+ * Expects the buildable to have a spline
  */
 UCLASS()
 class AB_XMASBOOSTER_API AABCurveBeamHologram : public AFGBeamHologram
 {
 	GENERATED_BODY()
 
-		AABCurveBeamHologram();
+public:
+	AABCurveBeamHologram();
+
+	// Begin AFGHologram interface
+	//virtual bool IsValidHitResult(const FHitResult& hitResult) const override;
+	//virtual bool TrySnapToActor(const FHitResult& hitResult) override;
+	//virtual void SetHologramLocationAndRotation(const FHitResult& hitResult) override;
+	virtual bool DoMultiStepPlacement(bool isInputFromARelease) override;
+	//virtual int32 GetRotationStep() const override;
+	virtual void GetSupportedBuildModes_Implementation(TArray< TSubclassOf<UFGHologramBuildModeDescriptor> >& out_buildmodes) const override;
+	virtual void ConfigureActor(AFGBuildable* inBuildable) const override;
+	//virtual int32 GetBaseCostMultiplier() const override;
+	//virtual bool CanBeZooped() const override;
+	//virtual bool CanIntersectWithDesigner(AFGBuildableBlueprintDesigner* designer) override;
+	// End AFGHologram interface
+
+	//virtual void OnPendingConstructionHologramCreated_Implementation(AFGHologram* fromHologram) override;
+
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Hologram|BuildMode")
+		TSubclassOf< class UFGHologramBuildModeDescriptor > mBuildModeCurved;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Hologram|BuildMode")
+		TSubclassOf< class UFGHologramBuildModeDescriptor > mBuildModeCompoundCurve;
+
+	// Custom:
+	bool bTangentInSet = false;
+	bool bTangentOutSet = false;
 };
