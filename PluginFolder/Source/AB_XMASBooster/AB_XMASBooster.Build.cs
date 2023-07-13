3885cdf276d6ac51c59834993896f9c5ee7fc0b0
davidg <david.h.gillen@gmail.com>
2023-07-12 18:10:30 -0600

    Updated build file

--- PluginFolder/Source/AB_XMASBooster/AB_XMASBooster.Build.cs
+++ PluginFolder/Source/AB_XMASBooster/AB_XMASBooster.Build.cs
@@ -10,34 +10,46 @@ public class AB_XMASBooster : ModuleRules
 	public AB_XMASBooster(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-            "Core", "CoreUObject",
-            "Engine",
-            "DeveloperSettings",
-            "PhysicsCore",
-            "InputCore",
-            "OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemUtils",
-            "SignificanceManager",
-            "GeometryCollectionEngine",
-            "ChaosVehiclesCore", "ChaosVehicles", "ChaosSolverEngine",
-            "AnimGraphRuntime",
-            "AkAudio",
-            "AssetRegistry",
-            "NavigationSystem",
-            "ReplicationGraph",
-            "AIModule",
-            "GameplayTasks",
-            "SlateCore", "Slate", "UMG",
-            "InstancedSplines",
-            "RenderCore",
-            "CinematicCamera",
-            "Foliage",
-            "Niagara",
-            "EnhancedInput",
-            "GameplayCameras",
-            "TemplateSequence",
-            "NetCore",
-            "GameplayTags",
+		PublicDependencyModuleNames.AddRange(new[] {
+			"Core", "CoreUObject",
+			"Engine",
+			"DeveloperSettings",
+			"PhysicsCore",
+			"InputCore",
+			"OnlineSubsystem", "OnlineSubsystemNull", "OnlineSubsystemUtils",
+			"SignificanceManager",
+			"GeometryCollectionEngine",
+			"ChaosVehiclesCore", "ChaosVehicles", "ChaosSolverEngine",
+			"AnimGraphRuntime",
+			"AkAudio",
+			"AssetRegistry",
+			"NavigationSystem",
+			"ReplicationGraph",
+			"AIModule",
+			"GameplayTasks",
+			"SlateCore", "Slate", "UMG",
+			"InstancedSplines",
+			"RenderCore",
+			"CinematicCamera",
+			"Foliage",
+			"Niagara",
+			"EnhancedInput",
+			"GameplayCameras",
+			"TemplateSequence",
+			"NetCore",
+			"GameplayTags",
+		});
+
+		// FactoryGame plugins
+		PublicDependencyModuleNames.AddRange(new[] {
+			"AbstractInstance",
+			"InstancedSplinesComponent",
+			"SignificanceISPC"
+		});
+
+		// Header stubs
+		PublicDependencyModuleNames.AddRange(new[] {
+			"DummyHeaders",
 		});
 
 
